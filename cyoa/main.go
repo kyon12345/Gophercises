5b2925dd7033995865a5115a5c71348dc7c3be09
yon <yu-g@luxondata.com>
2022-11-11 12:42:50 +0800

    cyoa

--- cyoa/main.go
+++ cyoa/main.go
@@ -1,75 +1,115 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"io/ioutil"
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"strings"
-	"text/template"
+	cyoa "cyoa/story"
 )
 
-type AdventureVM struct {
-	Title   string
-	Story   []string
-	Options []Opt
-}
-
-type Opt struct {
-	Text string
-	Arc  string
-}
-
-type Node struct {
-	Title   string   `json:"title"`
-	Story   []string `json:"story"`
-	Options []Opt    `json:"options"`
-	next    *Node    `json:"-"`
-}
-
 func main() {
-	var filePath string
-	flag.StringVar(&filePath, "f", "./gopher.json", "json location")
+	//create flags for our optional variables
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
+	filename := flag.String("file", "gopher.json", "thr JSON file with the CYOA story")
 	flag.Parse()
-	//parse json
-	fileByte, err := ioutil.ReadFile(filePath)
+	fmt.Printf("Using story in %s .\n", *filename)
+
+	//open JSON file and parse the story in it
+	f, err := os.Open(*filename)
 	if err != nil {
-		log.Fatal("fail to read file", err)
+		panic(err)
+	}
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		panic(err)
 	}
 
-	var list map[string]Node
-	if err := json.Unmarshal(fileByte, &list); err != nil {
-		log.Fatal("fail to decode json", err)
-	}
+	//create our custom CYOA story handler
+	tpl := template.Must(template.New("").Parse(storyTmpl))
+	h := cyoa.NewHandler(story,
+		cyoa.WithTemplate(tpl),
+		cyoa.WithPathFunc(pathFn))
 
-	//intro
-	intro := list["intro"]
-	tmpl := template.Must(template.ParseFiles("layout.html"))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
-		}
-		data := AdventureVM{
-			Story:   intro.Story,
-			Title:   intro.Title,
-			Options: intro.Options,
-		}
-		tmpl.Execute(w, data)
-	})
-
-	//parse
-	http.HandleFunc("/node/", func(w http.ResponseWriter, r *http.Request) {
-		key := strings.TrimPrefix(r.URL.Path, "/node/")
-		node := list[key]
-		data := AdventureVM{
-			Story:   node.Story,
-			Title:   node.Title,
-			Options: node.Options,
-		}
-		tmpl.Execute(w, data)
-	})
-
-	http.ListenAndServe(":9090", nil)
+	//create a ServerMux to route our requests
+	mux := http.NewServeMux()
+	//whose path has the /story/ prefix will be routed here
+	mux.Handle("/story/", h)
+	mux.Handle("/", cyoa.NewHandler(story))
+	fmt.Printf("Starting the server on port %d", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
 }
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+	return path[len("/story"):]	
+}
+
+// Slightly altered tempalte to show how this feature works
+var storyTmpl = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <section class="page">
+      <h1>{{.Title}}</h1>
+      {{range .Paragraphs}}
+        <p>{{.}}</p>
+      {{end}}
+      <ul>
+      {{range .Options}}
+        <li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+      {{end}}
+      </ul>
+    </section>
+    <style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FCF6FC;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #797;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: underline;
+        color: #555;
+      }
+      a:active,
+      a:hover {
+        color: #222;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
+  </body>
+</html>`
